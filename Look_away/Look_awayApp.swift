7901a0fdbe83e915a88a027d89046278e2fc6937
tejusna <tejusnagdev@gmail.com>
2026-06-27 23:28:20 +0530

    Initial Commit

---
+++ Look_away/Look_awayApp.swift
@@ -0,0 +1,17 @@
+//
+//  Look_awayApp.swift
+//  Look_away
+//
+//  Created by Tejus Nagdev on 27/06/26.
+//
+
+import SwiftUI
+
+@main
+struct Look_awayApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
